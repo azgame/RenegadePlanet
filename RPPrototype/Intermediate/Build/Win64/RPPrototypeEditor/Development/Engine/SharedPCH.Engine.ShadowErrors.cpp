1c6c96031b13952c7ca65262346852b883a46e7f
Aidan Zizys <43023816+azgame@users.noreply.github.com>
2020-04-20 12:09:16 -0400

    init ue4 project

---
+++ RPPrototype/Intermediate/Build/Win64/RPPrototypeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,1 @@
+#include "C:/Users/aidan/Documents/RenegadePlanet/RPPrototype/Intermediate/Build/Win64/RPPrototypeEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ RPPrototype/Intermediate/Build/Win64/UE4Editor/Inc/RPPrototype/RPPrototypeCharacter.generated.h
@@ -0,0 +1,97 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef RPPROTOTYPE_RPPrototypeCharacter_generated_h
+#error "RPPrototypeCharacter.generated.h already included, missing '#pragma once' in RPPrototypeCharacter.h"
+#endif
+#define RPPROTOTYPE_RPPrototypeCharacter_generated_h
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_SPARSE_DATA
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_RPC_WRAPPERS
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesARPPrototypeCharacter(); \
+	friend struct Z_Construct_UClass_ARPPrototypeCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ARPPrototypeCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/RPPrototype"), NO_API) \
+	DECLARE_SERIALIZER(ARPPrototypeCharacter)
+
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesARPPrototypeCharacter(); \
+	friend struct Z_Construct_UClass_ARPPrototypeCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ARPPrototypeCharacter, ACharacter, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/RPPrototype"), NO_API) \
+	DECLARE_SERIALIZER(ARPPrototypeCharacter)
+
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ARPPrototypeCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ARPPrototypeCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARPPrototypeCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARPPrototypeCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARPPrototypeCharacter(ARPPrototypeCharacter&&); \
+	NO_API ARPPrototypeCharacter(const ARPPrototypeCharacter&); \
+public:
+
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ARPPrototypeCharacter(ARPPrototypeCharacter&&); \
+	NO_API ARPPrototypeCharacter(const ARPPrototypeCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ARPPrototypeCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ARPPrototypeCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ARPPrototypeCharacter)
+
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(ARPPrototypeCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(ARPPrototypeCharacter, FollowCamera); }
+
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_9_PROLOG
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_SPARSE_DATA \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_RPC_WRAPPERS \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_INCLASS \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_SPARSE_DATA \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> RPPROTOTYPE_API UClass* StaticClass<class ARPPrototypeCharacter>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID RPPrototype_Source_RPPrototype_RPPrototypeCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
