5bc557340039a38908b96f0b192de6f2b5511938
azgame <azizysgame@gmail.com>
2020-05-08 14:46:32 -0400

    hotfix: loading broken

--- RPPrototype/Source/RPPrototype/Drill.cpp
+++ RPPrototype/Source/RPPrototype/Drill.cpp
@@ -6,48 +6,50 @@
 
 ADrill::ADrill()
 {
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> DrillAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cone"));
+	
+	if (DrillAsset.Succeeded())
+	{
+		Super::mesh->SetStaticMesh(DrillAsset.Object);
+		Super::mesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+		Super::mesh->SetWorldScale3D(FVector(1.0f));
+	}
 
-static ConstructorHelpers::FObjectFinder<UStaticMesh> DrillAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cone.Shape_Cone"));
-if (DrillAsset.Succeeded())
-{
-	
-	
-	
-	Super::mesh->SetStaticMesh(DrillAsset.Object);
-	Super::mesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
-	Super::mesh->SetWorldScale3D(FVector(1.0f));
+	CollisionMesh = CreateDefaultSubobject<UBoxComponent>(FName("Collision Mesh"));
+	CollisionMesh->SetupAttachment(RootComponent);
+	CollisionMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
+	CollisionMesh->SetRelativeScale3D(FVector(1.2f, 1.2f, 1.8f));
+	Super::pChargeTime=60;
+	Super::sChargeTime=60;
 }
-CollisionMesh = CreateDefaultSubobject<UBoxComponent>(FName("Collision Mesh"));
-CollisionMesh->SetupAttachment(RootComponent);
-CollisionMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
-CollisionMesh->SetRelativeScale3D(FVector(1.2f, 1.2f, 1.8f));
-Super::pChargeTime=60;
-Super::sChargeTime=60;
-}
+
 void ADrill::BeginPlay() 
 {
 	Super::BeginPlay();
 	CollisionMesh->SetRelativeLocation(FVector(0.0f, 0.0f, 50.0f));
 }
+
 void ADrill::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
+
 void ADrill::PAttack1()
 {
 	UE_LOG(LogTemp, Warning, TEXT("PA1"));
-	
-
 }
+
 void ADrill::PAttack2()
 {
 	UE_LOG(LogTemp, Warning, TEXT("PA2"));
 }
+
 void ADrill::SAttack1()
 {
 	UE_LOG(LogTemp, Warning, TEXT("SA1"));
 }
+
 void ADrill::SAttack2()
 {
-UE_LOG(LogTemp, Warning, TEXT("SA2"));
+	UE_LOG(LogTemp, Warning, TEXT("SA2"));
 }
